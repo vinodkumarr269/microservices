f3a2a91d0c2aa7870cff159b8a054200558cb553
vinodkumarr269 <55128041+vinodkumarr269@users.noreply.github.com>
2019-10-24 11:30:12 +0530

    Add files via upload

---
+++ microservice/accounts-microservice-server-master/src/main/java/com/hcl/ms/accounts/AccountRepository.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.hcl.ms.accounts;
+
+import java.util.List;
+
+/**
+ * @author Dinesh.Rajput
+ *
+ */
+public interface AccountRepository {
+	
+	List<Account> getAllAccounts();
+	
+	Account getAccount(String number);
+}
